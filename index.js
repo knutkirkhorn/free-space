7acbca1d612ccb9051b5ed13d76ce50eb861fdae
Knut Kirkhorn <knutkirk@hotmail.com>
2018-02-01 17:32:26 +0100

    Use default drive for all OSes if no input (fixes #2)

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 'use strict';
+const systemDisk = require('system-disk');
 const windows = require('./lib/windows.js');
 const unix = require('./lib/unix.js');
 
 module.exports = disk => {
     if (disk === undefined) {
-        if (process.platform === 'win32') {
-            //TODO: get the default disk
-            //temporary set it to c: as it is common for windows drive
-            disk = 'c';
-        } else {
-            disk = '';
-        }   
-    }
-
-    if (typeof disk !== 'string') {
-        throw new Error('Invalid input');
-        return;
-    }
-
-    if (process.platform === 'win32') {
-        return windows(disk);
+        return new Promise((resolve, reject) => {
+            systemDisk().then(newDisk => {
+                disk = newDisk;
+    
+                if (process.platform === 'win32') {
+                    resolve(windows(disk));
+                } else {
+                    resolve(unix(disk));
+                }
+            });
+        });
     } else {
-        return unix(disk);
+        if (typeof disk !== 'string') {
+            throw new TypeError('Invalid input');
+            return;
+        }
+    
+        if (process.platform === 'win32') {
+            return windows(disk);
+        } else {
+            return unix(disk);
+        }
     }
 };
